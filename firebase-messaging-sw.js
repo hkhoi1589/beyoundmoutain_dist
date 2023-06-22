b224fa0a849b6b9b79637349380edaf5876f2334
hkhoi1589 <hkhoi1589@gmail.com>
2023-06-22 12:42:28 +0700

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,32 +1,41 @@
 // Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+// eslint-disable-next-line no-undef
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js");
+// eslint-disable-next-line no-undef
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging.js"
+);
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-	apiKey: 'AIzaSyAkfHBzGYQcUZL0S76E3TukmzFuzCidge4',
-	authDomain: 'koolyard-84a4a.firebaseapp.com',
-	projectId: 'koolyard-84a4a',
-	storageBucket: 'koolyard-84a4a.appspot.com',
-	messagingSenderId: '669707854180',
-	appId: '1:669707854180:web:8a5e8aa2dd09025df95a71',
-	measurementId: 'G-PG9PCKGMHY',
+  apiKey: "AIzaSyBPv5xdqnMIpKr0JdqjetA2aoENywQ2h3E",
+  authDomain: "react-push-notification-ecffb.firebaseapp.com",
+  projectId: "react-push-notification-ecffb",
+  storageBucket: "react-push-notification-ecffb.appspot.com",
+  messagingSenderId: "911170895171",
+  appId: "1:911170895171:web:34a1ecbdeaa1b54ce743f6",
+  measurementId: "G-4V86PBRCFY",
 };
 
+// eslint-disable-next-line no-undef
 firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
+// eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-	console.log('Received background message ', payload);
+// messaging.onBackgroundMessage(function (payload) {
+//   console.log("Received background message ", payload);
 
-	const notificationTitle = payload.notification.title;
-	const notificationOptions = {
-		body: payload.notification.body,
-		icon: '/logo192.png',
-	};
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: "/logo192.png",
+//   };
 
-	// eslint-disable-next-line no-restricted-globals
-	return self.registration.showNotification(notificationTitle, notificationOptions);
-});
+//   // eslint-disable-next-line no-restricted-globals
+//   return self.registration.showNotification(
+//     notificationTitle,
+//     notificationOptions
+//   );
+// });
