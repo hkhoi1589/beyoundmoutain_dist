3bab77cb3938c8a5d0a94706715501efe0dc9fcc
hkhoi1589 <hkhoi1589@gmail.com>
2023-06-22 16:30:24 +0700

    fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
@@ -20,7 +20,7 @@ const messaging = firebase.messaging();
 
 //background notifications will be received here
 messaging.setBackgroundMessageHandler(function (payload) {
-	console.log('[firebase-messaging-sw.js] Received background message ', payload);
+	console.log('Received background message ', payload);
 	// Customize notification here
 	const notificationTitle = 'Background Message Title';
 	const notificationOptions = {
@@ -32,7 +32,7 @@ messaging.setBackgroundMessageHandler(function (payload) {
 });
 
 messaging.onBackgroundMessage(function (payload) {
-	console.log('[firebase-messaging-sw.js] Received background message ', payload);
+	console.log('Received background message ', payload);
 	const notificationTitle = 'Background Message Title';
 	const notificationOptions = {
 		body: 'Background Message body.',
