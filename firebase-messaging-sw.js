ad041d9fce8605db5788eb66eff0f7a6da94b029
hkhoi1589 <hkhoi1589@gmail.com>
2023-06-22 17:47:43 +0700

    fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,7 @@
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js');
+
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
 	apiKey: 'AIzaSyAkfHBzGYQcUZL0S76E3TukmzFuzCidge4',
@@ -9,19 +13,21 @@ const firebaseConfig = {
 	measurementId: 'G-PG9PCKGMHY',
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+	firebase.initializeApp(firebaseConfig);
+}
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-	console.log('Received background message ', payload);
-
-	const notificationTitle = payload.notification.title;
-	const notificationOptions = {
-		body: payload.notification.body,
-		icon: '/logo192.png',
-	};
-
-	return self.registration.showNotification(notificationTitle, notificationOptions);
+messaging.onBackgroundMessage(async (message) => {
+	if (Notification.permission === 'granted') {
+		if (navigator.serviceWorker)
+			navigator.serviceWorker.getRegistration().then(async function (reg) {
+				if (reg)
+					await reg.showNotification(message.notification.title, {
+						body: message.notification.body,
+					});
+			});
+	}
 });
