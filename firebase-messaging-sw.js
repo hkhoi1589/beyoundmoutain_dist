e438c66aeae7419dd6b3fdfce75b419ef5b1d82a
hkhoi1589 <hkhoi1589@gmail.com>
2023-06-22 16:34:40 +0700

    fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 // Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js');
+// eslint-disable-next-line no-undef
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
@@ -18,26 +19,28 @@ firebase.initializeApp(firebaseConfig);
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-//background notifications will be received here
-messaging.setBackgroundMessageHandler(function (payload) {
-	console.log('Received background message ', payload);
-	// Customize notification here
-	const notificationTitle = 'Background Message Title';
-	const notificationOptions = {
-		body: 'Background Message body.',
-		icon: '/firebase-logo.png',
-	};
+// //background notifications will be received here
+// messaging.setBackgroundMessageHandler(function (payload) {
+// 	console.log('Received background message ', payload);
+// 	// Customize notification here
+// 	const notificationTitle = 'Background Message Title';
+// 	const notificationOptions = {
+// 		body: 'Background Message body.',
+// 		icon: '/firebase-logo.png',
+// 	};
 
-	return self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// 	return self.registration.showNotification(notificationTitle, notificationOptions);
+// });
 
 messaging.onBackgroundMessage(function (payload) {
 	console.log('Received background message ', payload);
-	const notificationTitle = 'Background Message Title';
+
+	const notificationTitle = payload.notification.title;
 	const notificationOptions = {
-		body: 'Background Message body.',
-		icon: '/firebase-logo.png',
+		body: payload.notification.body,
+		icon: '/logo192.png',
 	};
 
+	// eslint-disable-next-line no-restricted-globals
 	return self.registration.showNotification(notificationTitle, notificationOptions);
 });
