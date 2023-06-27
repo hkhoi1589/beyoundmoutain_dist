2e9d27012f6e3b0370f80156baa2c5359bf0dba9
hkhoi1589 <hkhoi1589@gmail.com>
2023-06-27 10:29:12 +0700

    demo fcm

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-comp
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-	apiKey: 'AIzaSyBPv5xdqnMIpKr0JdqjetA2aoENywQ2h3E',
-	authDomain: 'react-push-notification-ecffb.firebaseapp.com',
-	projectId: 'react-push-notification-ecffb',
-	storageBucket: 'react-push-notification-ecffb.appspot.com',
-	messagingSenderId: '911170895171',
-	appId: '1:911170895171:web:34a1ecbdeaa1b54ce743f6',
-	measurementId: 'G-4V86PBRCFY',
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
@@ -28,6 +28,7 @@ messaging.onBackgroundMessage(async (message) => {
 				if (reg)
 					await reg.showNotification(message.notification.title, {
 						body: message.notification.body,
+						icon: '/favicon.ico',
 					});
 			});
 	}
